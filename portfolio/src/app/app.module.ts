08aa86b3b8428b438e84cb39c65b4d7d6ce4ad9d
Juliana <juulyjas@gmail.com>
2023-03-15 15:21:34 -0300

    creacion componente logo ap y social

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule

20c0a94782f8c7a5fc64ac0fe1fef83af1b49a30
Juliana <juulyjas@gmail.com>
2023-03-18 16:56:21 -0300

    Modificaciones del Acerca De y creacion del componente experiencia

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoApComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule

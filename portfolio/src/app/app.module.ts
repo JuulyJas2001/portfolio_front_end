4a4b6006700e455335237d4e38a479e247a8068c
Juliana <juulyjas@gmail.com>
2023-03-18 22:14:29 -0300

    Educacion

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -8,19 +8,18 @@ import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent,
-    SocialComponent,
-    BannerComponent,
-    AcercaDeComponent,
-    ExperienciaComponent
+    EducacionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]

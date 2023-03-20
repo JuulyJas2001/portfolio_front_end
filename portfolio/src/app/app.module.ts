9740d64aa24a92e85ffb428c83f21a960fbff7c4
Juliana <juulyjas@gmail.com>
2023-03-20 20:40:01 -0300

    Terminacion inicial del front-end

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -18,7 +17,7 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent,
+    
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
